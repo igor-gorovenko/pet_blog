ad45d2ac65d9e70276f46164baa3b5873dd0d869
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-06-16 11:41:43 +0300

    Update models, create admin

--- pet_blog_project/articles/models.py
+++ pet_blog_project/articles/models.py
@@ -1,3 +1,5 @@
+import datetime
+from django.utils import timezone
 from django.db import models
 
 
@@ -6,9 +8,18 @@ class Article(models.Model):
     article_text = models.TextField()
     creation_date = models.DateField()
 
+    def __str__(self) -> str:
+        return self.title
+    
+    def was_created_recently(self):
+        return self.creation_date >= timezone.now() - datetime.timedelta(days=1) 
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     age = models.SmallIntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
 
