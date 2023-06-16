4b3cbbc4679dae6765e260aae1fab7ba4bc1748e
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-06-16 11:14:31 +0300

    create models Article, Author

--- pet_blog_project/articles/models.py
+++ pet_blog_project/articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    article_text = models.TextField()
+    creation_date = models.DateField()
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    age = models.SmallIntegerField()
+
