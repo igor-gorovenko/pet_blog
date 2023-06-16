6999086ea00e57d2997590fb2457711482191780
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-06-16 13:59:33 +0300

    add templates

--- pet_blog_project/articles/views.py
+++ pet_blog_project/articles/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Article, Author
 
 
 def index(request):
-    return HttpResponse("This is articles")
+    list_articles = Article.objects.order_by("-creation_date")[:5]
+    context = {
+        'list_articles': list_articles,
+    }
+    return render(request, 'articles/index.html', context)
 
-# Create your views here.
+
+def article(request, id):
+    article = get_object_or_404(Article, pk=id)
+    return render(request, 'articles/detail.html', {'article': article})
+
+
+def author(page, id):
+    try:
+        author = Author.objects.get(pk=id)
+    except Author.DoesNotExist:
+        raise Http404('Author does not exist')
+    return HttpResponse(request, 'authors/author', {'author': author})
