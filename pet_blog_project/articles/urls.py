7e28030b36f934ff14b1d0bdfcb789cb590689a9
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-06-19 15:11:17 +0300

    add tags filter

--- pet_blog_project/articles/urls.py
+++ pet_blog_project/articles/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.article, name='detail'),
+    path('tag/<slug:tag_slug>', views.articles_by_tag, name='tag'),
 ]
--- pet_blog_project/articles/views.py
+++ pet_blog_project/articles/views.py
@@ -17,4 +17,21 @@ def index(request):
 
 def article(request, id):
     article = get_object_or_404(Article, pk=id)
-    return render(request, 'detail.html', {'article': article})
+    list_tags = article.tags.all()
+    context = {
+        'article': article,
+        'list_tags': list_tags,
+        }
+    return render(request, 'detail.html', context)
+
+
+def articles_by_tag(request, tag_slug):
+    tag = get_object_or_404(Tag, slug=tag_slug)
+    list_articles = Article.objects.filter(tags__in=[tag])
+    list_tags = Tag.objects.all()
+    context = {
+        'list_articles': list_articles,
+        'list_tags': list_tags,
+    }
+    return render(request, 'tag.html', context)
+    
