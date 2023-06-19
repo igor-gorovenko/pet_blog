1babab581726af7d12142e00cbe68627bdd606c0
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-06-19 14:30:56 +0300

    upd sidebar

--- pet_blog_project/articles/views.py
+++ pet_blog_project/articles/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Article
+from .models import Article, Tag, Author
 
 
 def index(request):
     list_articles = Article.objects.all()
+    list_tags = Tag.objects.all()
+    list_authors = Author.objects.all()
     context = {
         'list_articles': list_articles,
+        'list_tags': list_tags,
+        'list_authors': list_authors,
     }
     return render(request, 'index.html', context)
 
