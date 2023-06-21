2e33a7c68b07ac1d9fded7b6dcb89cbd6191c15a
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-06-21 13:16:11 +0300

    fix

--- pet_blog_project/pet_blog_project/settings.py
+++ pet_blog_project/pet_blog_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_seed
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "articles.apps.ArticlesConfig",
+    'django_seed',
 ]
 
 MIDDLEWARE = [
